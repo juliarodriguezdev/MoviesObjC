7d376e85bd9fe29f486dee4f9b808ad1b05c5089
Julia Rodriguez <juliarodriguez@Julias-MacBook-Pro.local>
2019-07-05 09:42:30 -0600

    View scene set up

---
+++ MoviesObjc/MoviesObjc/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoviesObjc
+//
+//  Created by Julia Rodriguez on 7/5/19.
+//  Copyright © 2019 Julia Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
